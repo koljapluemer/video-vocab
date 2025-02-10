b9dd485787a8bb8d0f0549e7321bff6f68fe587c
blackbrokkoli <kastomat@gmail.com>
2025-02-10 20:31:04 +0100

    test more complex video component

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,12 @@
 <template>
   <LiteYouTubeEmbed id="Uuo2FTlsiug" title="Learning Next" />
+  <YouTube src="https://www.youtube.com/watch?v=jNQXAC9IVRw" ref="youtube" />
 </template>
 
 <script setup lang="ts">
+import YouTube from 'vue3-youtube'
 import LiteYouTubeEmbed from "vue-lite-youtube-embed";
+
 import "vue-lite-youtube-embed/style.css";
 
 </script>
